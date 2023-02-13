16fa647852199f0683fc14c7ac70c0e85d6ec56f
Kenny Johnson <kenny.johnson.nyc@gmail.com>
2023-02-13 09:40:49 -0500

    Basic functionality working

--- pages/Product.tsx
+++ pages/Product.tsx
@@ -1,19 +1,24 @@
 // pages/Product.tsx
 import React, { useState } from 'react';
+import styles from '@/styles/Home.module.css'
 
+// Define the type of props that the component will receive
 interface Props {
   name: string;
   count: number;
 }
 
+
 const Product: React.FC<Props> = ({ name, count }) => {
   const [quantity, setQuantity] = useState(count);
 
   return (
-    <div>
-      <h2>{name}</h2>
+    <div className={styles.product}>
+      <h2 className={styles.productName}>{name}</h2>
       <p>Count: {quantity}</p>
+    
       <button onClick={() => setQuantity(quantity + 1)}>Increase</button>
+
       <button onClick={() => setQuantity(quantity - 1)}>Decrease</button>
     </div>
   );
