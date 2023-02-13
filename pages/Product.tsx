614e26079bafd75824c5afdd0c7330bca00f8d75
Kenny Johnson <kenny.johnson.nyc@gmail.com>
2023-02-13 11:26:23 -0500

    Some styling and unique images for each card

--- pages/Product.tsx
+++ pages/Product.tsx
@@ -1,32 +1,51 @@
 // pages/Product.tsx
 import React, { useState } from 'react';
 import styles from '@/styles/Home.module.css'
-import { Card, Text } from '@nextui-org/react';
+import { Card, Text, Button, Container, Image } from '@nextui-org/react';
 
 
 // Define the type of props that the component will receive
 interface Props {
     name: string;
     count: number;
+    imgURL: string;
 }
 
 
-const Product: React.FC<Props> = ({ name, count }) => {
+const Product: React.FC<Props> = ({ name, count, imgURL }) => {
     const [quantity, setQuantity] = useState(count);
-
+    const [image, setImage] = useState(imgURL);
+ 
     return (
         <>
-            <Card isHoverable variant="bordered" css={{ mw: "400px" }}>
+            <Card isHoverable variant="bordered" css={{ 
+                mw: "400px", 
+                mb: "1rem" ,
+                bg: "linear-gradient(19deg, rgba(2,0,100,.3) 0%, rgba(9,9,121,1) 0%, rgba(0,255,255,.6) 100%)",
+                color: "white",
+                borderRadius: "10px",
+                boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+                p: "1rem",
+                m: "1rem",
+                fontSize: "1.5rem",
+                fontWeight: "600",
+                textAlign: "center",
+                textTransform: "uppercase",
+                lineHeight: "1.5"
+                }}>
                 <Card.Header className={styles.productName}>{name}</Card.Header>
                 <Card.Body>
                     <Text>Count: {quantity}</Text>
+                    <Image src={image} width="100px" height="100px" />
+                    <Container>
+                        <Button onPress={() => setQuantity(quantity + 1)}>Increase</Button>
+                    </Container>
 
-                    <button onClick={() => setQuantity(quantity + 1)}>Increase</button>
-
-                    <button onClick={() => setQuantity(quantity - 1)}>Decrease</button>
+                    <Container>
+                        <Button onPress={() => setQuantity(quantity - 1)}>Decrease</Button>
+                    </Container>
                 </Card.Body>
             </Card>
-            <Card.Divider></Card.Divider>
         </>
     );
 };
--- pages/Products.tsx
+++ pages/Products.tsx
@@ -4,20 +4,22 @@ import Product from './Product';
 import style from '@/styles/Home.module.css'
 
 const Products: React.FC = () => {
-  const [products, setProducts] = useState([
-    { name: 'Bananas', count: 5 },
-    { name: 'Apples', count: 10 },
-    { name: 'Oranges', count: 7 },
-  ]);
+    // Define the type of props that the component will receive and set the initial state
+    const [products] = useState([
+        { name: 'Bananas', count: 5, imgURL: "https://www.seekpng.com/png/full/24-249030_banana-png-free-commercial-use-images-banana.png" },
+        { name: 'Apples', count: 10, imgURL: 'https://www.nicepng.com/png/detail/153-1537482_clip-freeuse-apples-transparent-top-red-apple.png' },
+        { name: 'Oranges', count: 7, imgURL: 'https://www.publicdomainpictures.net/pictures/10000/velka/orange-871282749123hSB3.jpg' },
+    ]);
 
-  return (
-    <div className={style.container}>
-      <h1 className={style.title}>Products</h1>
-      {products.map((product) => (
-        <Product key={product.name} name={product.name} count={product.count} />
-      ))}
-    </div>
-  );
+    return (
+        <>
+            <h1 className={style.title}>Products</h1>
+        
+            {products.map((product) => (
+                <Product key={product.name} name={product.name} count={product.count} imgURL={product.imgURL} />
+            ))}
+        </>
+    );
 };
 
 export default Products;
