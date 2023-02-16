0bf5fd5b7f9bea46774acee23e24495db7a8e286
Kenny Johnson <kenny.johnson.nyc@gmail.com>
2023-02-16 16:59:41 -0500

    Grid added to Products, stable

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,21 @@ import { Navbar } from  "@nextui-org/react";
 
 export default function Nav() {
     return (
-        <Navbar>
+        <Navbar css={{
+            margin: "1rem",
+            padding: "1.5rem",
+            borderRadius: "10px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+            fontSize: "1.5rem",
+            fontWeight: "600",
+            textAlign: "center",
+            textTransform: "uppercase",
+            lineHeight: "1.5",
+            backgroundColor: "white",
+            color: "black",
+            display: "flex",
+            justifyContent: "space-between",
+        }}>
             <Navbar.Brand >Inventory App</Navbar.Brand>
             <Navbar.Content>
                 <Navbar.Link href="/products">Products</Navbar.Link>
--- pages/Products.tsx
+++ pages/Products.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Product from './Product';
 import style from '@/styles/Products.module.css'
 import { ProductContext } from '@/components/ProductContext';
+import { Grid } from '@nextui-org/react';
 
 interface Props {
   product: {
@@ -33,15 +34,23 @@ const Products: React.FC = () => {
     <div className={style.products}>
       <div className={style.sortContainer}>
         <label htmlFor="sort-select">Sort by:</label>
-        <select id="sort-select" value={sortOption} onChange={handleSortChange}>
+        <select
+          id="sort-select"
+          value={sortOption}
+          onChange={handleSortChange}
+        >
           <option value="default">Default</option>
           <option value="count">Stock Count</option>
           <option value="title">Title</option>
         </select>
       </div>
-      {sortedProducts.map((product, index) => (
-        <Product key={index} product={product} />
-      ))}
+      <Grid.Container gap={2}>
+        {sortedProducts.map((product, index) => (
+          <Grid xs={12} sm={6} md={4} lg={3} key={index}>
+            <Product key={index} product={product} />
+          </Grid>
+        ))}
+      </Grid.Container>
     </div>
   );
 };
